0e8df0e0d2bd624b3998db2bdf3c5eee23a1433d
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-01-17 21:35:30 +0600

    Show and Index pages

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+	before_action :find_picture, only: [:show, :edit, :update, :destroy]
 
 	def index
 	end
@@ -9,12 +10,23 @@ class PicturesController < ApplicationController
 
 	def create
 		@picture = Picture.new(picture_params)
+
+		if @picture.save
+			redirect_to @picture, notice: "Successfully created!"
+		else
+			render 'new'
+		end
+
 	end
 
 	private
 
 		def picture_params
 			params.require(:picture).permit(:title, :description)
+		end
+
+		def find_picture
+			@picture = Picture.find(params[:id])
 		end
 
 end
