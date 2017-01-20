982f4ead8198e53ca994e340bd303a7f273ccec0
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-01-20 17:26:20 +0600

    Added acts_as_votable gem and setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  resources :pictures
+
+  resources :pictures do
+    member do
+      put "like", to: "pictures#upvote"
+    end
+  end
+
   root "pictures#index"
 
 end
