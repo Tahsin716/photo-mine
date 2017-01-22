d7fae80e70842cc3c429622b3b7ef48106bb7c65
Tahsin Rashad <tahsin.rashad716@gmail.com>
2017-01-22 20:40:03 +0600

    Imagemick and view files debugged

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,12 @@
 class Picture < ActiveRecord::Base
+	validates :title, presence: true
+	validates :description, presence: true
+    validates :image_file_name, presence: true
+
+
 	acts_as_votable
 	belongs_to :user
 
-	has_attached_file :image, styles: { medium: "300x300>" }
-  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+	has_attached_file :image, :styles => { :medium => "300x300>" }
+  	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
 end
